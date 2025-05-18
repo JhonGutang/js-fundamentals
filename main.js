72b9b0b3bf4b5893c75e348e45a15881f0ce99de
Jhon Barry Gutang <jbgutang771@gmail.com>
2025-05-18 22:42:28 +0800

    adding more lessons i learned in fundamentals

--- main.js
+++ main.js
@@ -1,8 +1,58 @@
-
 // Dynamically Typed
 let value = 30;
-value = true
+value = true;
 console.log(typeof value);
 
+// correct Hoisting
+console.log(greet());
+function greet() {
+  return "Hello this is hoisting";
+}
 
-// Hoisting
+// incorrect hoisting
+// console.log(saySomething());
+
+const saySomething = () => {
+  return "Hello this is incorrect hoisting";
+};
+
+// function expression
+
+function greetSomeone(name) {
+  console.log("Hello " + name);
+}
+
+greetSomeone("Aira");
+
+// High Order Function, calling a function inside a function
+
+function birthdayGreet(callback) {
+  console.log(callback());
+}
+
+birthdayGreet(greet);
+
+// arrow function
+const greetHer = () => {
+  return "Hello babe";
+};
+console.log(greetHer());
+
+// getters setters
+class Person  {
+    constructor(name) {
+        this.name = name
+    }
+
+    getName() {
+        return 'This is ' + this.name
+    }
+    setName(name) {
+        this.name = name
+    }
+}
+
+const person1 = new Person('jhon')
+console.log(person1.getName());
+person1.setName('Sebastian')
+console.log(person1.getName());
